1cfc7031367ae5f09b1ae843eafabba307f5d13b
willyyooan <wilson.yuan98@gmail.com>
2021-02-28 20:56:52 -0800

    added randomness in starting shape/task

--- Diffuser_v1.py
+++ Diffuser_v1.py
@@ -1,9 +1,9 @@
 from sense_hat import SenseHat
 from time import sleep
+import random
 sh = SenseHat()
 
 sh.clear()
-sh.low_light(True)
 
 ##COLOURS##
 r = [255, 0, 0] #red
@@ -67,7 +67,8 @@ triangleScreen = [
     x, x, w, x, x, w, x, x,
     x, w, x, x, x, x, w, x,
     w, x, x, x, x, x, x, w,
-    w, w, w, w, w, w, w, w
+    w, w, w, w, w, w, w, w,
+    x, x, x, x, x, x, x, x
 ]
 
 circleScreen = [
@@ -159,74 +160,85 @@ xScreen = [
 ]
 
 ##VARIABLES AND BOOLEANS
-global isGameOn = False
-global isGameLose = False
-global isGameWin = False
+isGameOn = False
+isGameLose = False
+isGameWin = False
 
 qrtrSec = 0.25
 halfSec = 0.5
 fullSec = 1
 
 ##FUNCTIONS##
+#def determineNextTask()
+
+def nextTask():
+    print("next task!")
+
 def taskDone():
     print("task done!")
     sh.set_pixels(flashScreen)
     sleep(halfSec)
     sh.set_pixels(flashScreen)
     sleep(halfSec)
-
-def nextTask():
-    print("next task!")
+    nextTask()
 
 def taskFailed():
     print("BOOM! You died :(")
     if isGameLose == True:
         sh.set_pixels(loseScreen)
-        sleep(qrtrSec)    
+        sleep(qrtrSec)
 
-#INSTRUCTIONS
-def triangle():
-    #Press the joystick 3 times
-    if isGameOn == True:
-        if sh.get_pixels(triangleScreen):
-            for e in sh.stick.get_events():
-                if e.action == 'pressed' and e.direction == "up":
-                    print("up")
-                    taskDone()
-                    nextTask()
-                else:
-                    isGameLose = True
-                    isGameOn = False
-                    taskFailed()
 
+
+
+
+
+
+     
 
 ##GAME##
 
 while True:
-    #Start screen before anything begins. Flashes 
-    #through all the shapes for aesthetic purposes :)
+    if isGameOn == False:
+        sh.set_pixels(flashScreen)
+        #MVP 1: press the joystick to begin 
+        #MVP 2: MQTT, the GUI will start the game
+        for e in sh.stick.get_events():
+            if e.action == "pressed" and "middle":
+                sh.clear()
+                isGameOn = True
+                break
+            break
+    elif isGameOn == True:
+      break
+
+
+taskArr = ["task_1", "task_2", "task_3", "task_4", "task_5", "task_6", "task_7", "task_8", "task_9", "task_10"]
+random.shuffle(taskArr)
+print(taskArr[0])
+
+if taskArr[0] == "task_1":
     sh.set_pixels(triangleScreen)
-    sleep(halfSec)
-    sh.set_pixels(circleScreen)
-    sleep(halfSec)
-    sh.set_pixels(blueScreen)
-    sleep(halfSec)
-    sh.set_pixels(twoSquareScreen)
-    sleep(halfSec)
-    sh.set_pixels(yellowScreen)
-    sleep(halfSec)
+    while isGameOn == True:
+        for e in sh.stick.get_events():
+            if e.action == "pressed" and "up"
+elif taskArr[0] == "task_2":
     sh.set_pixels(squareScreen)
-    sleep(halfSec)
+elif taskArr[0] == "task_3":
+    sh.set_pixels(circleScreen)
+elif taskArr[0] == "task_4":
+    sh.set_pixels(blueScreen)
+elif taskArr[0] == "task_5":
+    sh.set_pixels(twoSquareScreen)
+elif taskArr[0] == "task_6":
+    sh.set_pixels(yellowScreen)
+elif taskArr[0] == "task_7":
     sh.set_pixels(orangeScreen)
-    sleep(halfSec)
+elif taskArr[0] == "task_8":
     sh.set_pixels(crossScreen)
-    sleep(halfSec)
+elif taskArr[0] == "task_9":
     sh.set_pixels(twoLinesScreen)
-    sleep(halfSec)
+elif taskArr[0] == "task_10":
     sh.set_pixels(xScreen)
-    sleep(halfSec)
-    #MVP 1: press the joystick to begin 
-    #MVP 2: MQTT, the GUI will start the game
-    for e in sh.stick.get_events():
-        if e.action == 'pressed' and e.direction == "middle":
-            triangle()
+
+    
