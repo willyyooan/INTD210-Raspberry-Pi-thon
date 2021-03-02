2d69bd234afbeeafc358fa287b9af2e44cbe9f14
willyyooan <wilson.yuan98@gmail.com>
2021-03-01 18:41:31 -0800

    basic tasks sequence works!

---
+++ Diffuser_v2.py
@@ -0,0 +1,316 @@
+from sense_hat import SenseHat
+from time import sleep
+import random
+sh = SenseHat()
+
+sh.clear()
+
+##COLOURS##
+r = [255, 0, 0] #red
+g = [0, 255, 0] #green
+x = [0, 0, 0] #blank
+w = [255, 255, 255]
+B = [0, 0, 255] #Blue
+O = [255, 180, 0] #Orange
+Y = [255, 255, 0] #Yellow
+						
+
+##SCREENS##
+loseScreen = [
+    r, r, r, r, r, r, r ,r,
+    r, r, r, r, r, r, r ,r,
+    r, r, r, r, r, r, r ,r,
+    r, r, r, r, r, r, r ,r,
+    r, r, r, r, r, r, r ,r,
+    r, r, r, r, r, r, r ,r,
+    r, r, r, r, r, r, r ,r,
+    r, r, r, r, r, r, r ,r
+]
+
+winScreen = [
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g,
+    g, g, g, g, g, g, g, g
+]
+
+flashScreen = [
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w
+]
+
+squareScreen = [
+    x, x, x, x, x, x, x, x,
+    x, w, w, w, w, w, w, x,
+    x, w, x, x, x, x, w, x,
+    x, w, x, x, x, x, w, x,
+    x, w, x, x, x, x, w, x,
+    x, w, x, x, x, x, w, x,
+    x, w, w, w, w, w, w, x,
+    x, x, x, x, x, x, x, x
+]
+
+triangleScreen = [
+    x, x, x, x, x, x, x, x,
+    x, x, x, x, x, x, x, x,
+    x, x, x, w, w, x, x, x,
+    x, x, w, x, x, w, x, x,
+    x, w, x, x, x, x, w, x,
+    w, x, x, x, x, x, x, w,
+    w, w, w, w, w, w, w, w,
+    x, x, x, x, x, x, x, x
+]
+
+circleScreen = [
+    x, x, x, x, x, x, x, x,
+    x, x, x, w, w, x, x, x,
+    x, x, w, x, x, w, x, x,
+    x, w, x, x, x, x, w, x,
+    x, w, x, x, x, x, w, x,
+    x, x, w, x, x, w, x, x,
+    x, x, x, w, w, x, x, x,
+    x, x, x, x, x, x, x, x,
+]
+
+blueScreen = [
+    x, x, x, x, x, x, x, x,
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, B, B, B, B
+]
+
+twoSquareScreen = [
+    x, x, x, x, x, x, x, x,
+    w, w, w, w, x, x, x, x,
+    w, x, x, w, x, x, x, x,
+    w, x, x, w, x, x, x, x,
+    w, w, w, w, w, w, w, w,
+    x, x, x, x, w, x, x, w,
+    x, x, x, x, w, x, x, w,
+    x, x, x, x, w, w, w, w
+]
+
+yellowScreen = [
+    x, x, x, x, x, x, x, x,
+    Y, Y, Y, Y, Y, Y, Y, Y,
+    Y, Y, Y, Y, Y, Y, Y, Y,
+    Y, Y, Y, Y, Y, Y, Y, Y,
+    Y, Y, Y, Y, Y, Y, Y, Y,
+    Y, Y, Y, Y, Y, Y, Y, Y,
+    Y, Y, Y, Y, Y, Y, Y, Y,
+    Y, Y, Y, Y, Y, Y, Y, Y
+]
+
+orangeScreen = [
+    x, x, x, x, x, x, x, x,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O
+]
+
+crossScreen = [
+    x, x, x, x, x, x, x, x,
+    x, x, x, w, w, x, x, x,
+    x, x, x, w, w, x, x, x,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w,
+    x, x, x, w, w, x, x, x,
+    x, x, x, w, w, x, x, x,
+    x, x, x, w, w, x, x, x
+]
+
+twoLinesScreen = [
+    x, x, x, x, x, x, x, x,
+    x, x, x, x, x, x, x, x,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w,
+    x, x, x, x, x, x, x, x,
+    x, x, x, x, x, x, x, x,
+    w, w, w, w, w, w, w, w,
+    w, w, w, w, w, w, w, w
+]
+
+xScreen = [
+    x, x, x, x, x, x, x, x,
+    x, w, x, x, x, x, w, x,
+    x, x, w, x, x, w, x, x,
+    x, x, x, w, w, x, x, x,
+    x, x, x, w, w, x, x, x,
+    x, x, w, x, x, w, x, x,
+    x, w, x, x, x, x, w, x,
+    w, x, x, x, x, x, x, w
+]
+
+##VARIABLES AND BOOLEANS
+global isGameOn
+global isGameLose
+global isGameWin
+
+isGameOn = False
+isGameLose = False
+isGameWin = False
+
+qrtrSec = 0.25
+halfSec = 0.5
+fullSec = 1
+
+
+##FUNCTIONS##
+def nextTask():
+    sh.clear()
+    random.shuffle(taskArr)
+    print(taskArr[0])
+
+def taskDone():
+    print("task done! next task")
+    sh.set_pixels(flashScreen)
+    sleep(0.5)
+    
+
+def taskFailed():
+    isGameLose = True
+    print("BOOM! You died :(")
+    while isGameLose == True:
+        isGameOn = False
+        sh.set_pixels(loseScreen)
+
+def task(i):
+    sh.set_pixels(i)
+    if i == triangleScreen:
+        for e in sh.stick.get_events():
+            if e.action == "pressed" and e.direction == "up":
+                taskDone()
+                nextTask()
+            if e.action == "pressed" and e.direction != "up":
+                isGameLose = True
+                taskFailed()
+    elif i == squareScreen:
+        for e in sh.stick.get_events():
+            if e.action == "pressed" and e.direction == "left":
+                taskDone()
+                nextTask()
+            if e.action == "pressed" and e.direction != "left":
+                isGameLose = True
+                taskFailed()
+    elif i == circleScreen:
+        for e in sh.stick.get_events():
+            if e.action == "pressed" and e.direction == "right":
+                taskDone()
+                nextTask()
+            if e.action == "pressed" and e.direction != "right":
+                isGameLose = True
+                taskFailed()
+    elif i == blueScreen:
+        for e in sh.stick.get_events():
+            if e.action == "pressed" and e.direction == "down":
+                taskDone()
+                nextTask()
+            if e.action == "pressed" and e.direction != "down":
+                isGameLose = True
+                taskFailed()
+    elif i == twoSquareScreen:
+        for e in sh.stick.get_events():
+            if e.action == "pressed" and e.direction == "middle":
+                taskDone()
+                nextTask()
+            if e.action == "pressed" and e.direction != "middle":
+                isGameLose = True
+                taskFailed()
+
+
+
+
+
+
+
+##GAME##
+while True:
+    if isGameOn == False:
+        sh.set_pixels(flashScreen)
+        #MVP 1: press the joystick to begin 
+        #MVP 2: MQTT, the GUI will start the game
+        for e in sh.stick.get_events():
+            if e.action == "pressed" and e.direction == "middle":
+                sh.clear()
+                isGameOn = True
+                break
+            break
+    elif isGameOn == True:
+      break
+
+
+
+taskArr = ["task_1", "task_2", "task_3", "task_4", "task_5"]#, "task_6", "task_7", "task_8", "task_9", "task_10"]
+random.shuffle(taskArr)
+
+while isGameOn == True:
+    if taskArr[0] == "task_1": #up
+        task(triangleScreen) 
+        
+    elif taskArr[0] == "task_2": #left
+        task(squareScreen)
+
+    elif taskArr[0] == "task_3": #right
+        task(circleScreen)
+
+    elif taskArr[0] == "task_4": #down
+        task(blueScreen)
+
+    elif taskArr[0] == "task_5": #middle
+        task(twoSquareScreen)
+
+# elif taskArr[0] == "task_6":
+#     sh.set_pixels(yellowScreen)
+#     while isGameOn == True:
+#         for e in sh.stick.get_events():
+#             if e.action == "pressed" and "up":
+#                 print("task done, moving on")
+
+# elif taskArr[0] == "task_7":
+#     sh.set_pixels(orangeScreen)
+#     while isGameOn == True:
+#         for e in sh.stick.get_events():
+#             if e.action == "pressed" and "up":
+#                 print("task done, moving on")
+
+# elif taskArr[0] == "task_8":
+#     sh.set_pixels(crossScreen)
+#     while isGameOn == True:
+#         for e in sh.stick.get_events():
+#             if e.action == "pressed" and "up":
+#                 print("task done, moving on")
+
+# elif taskArr[0] == "task_9":
+#     sh.set_pixels(twoLinesScreen)
+#     while isGameOn == True:
+#         for e in sh.stick.get_events():
+#             if e.action == "pressed" and "up":
+#                 print("task done, moving on")
+
+# elif taskArr[0] == "task_10":
+#     sh.set_pixels(xScreen)
+#     while isGameOn == True:
+#         for e in sh.stick.get_events():
+#             if e.action == "pressed" and "up":
+#                 print("task done, moving on")
+
+
+print("out of loop")   
--- Test environment.py
+++ Test environment.py
@@ -1,32 +1,4 @@
 import random
 
-isGameOn = True
-
-
-taskArr = ["task_1", "task_2", "task_3", "task_4", "task_5", "task_6", "task_7", "task_8", "task_9", "task_10"]
-random.shuffle(taskArr)
-print(str(taskArr[0]))
-
-
-# print(randomSet())
-if taskArr[0] == "task_1":
-    print("1")
-elif taskArr[0] == "task_2":
-    print("2")
-elif taskArr[0] == "task_3":
-    print("3")
-elif taskArr[0] == "task_4":
-    print("4")
-elif taskArr[0] == "task_5":
-    print("5")
-elif taskArr[0] == "task_6":
-    print("6")
-elif taskArr[0] == "task_7":
-    print("7")
-elif taskArr[0] == "task_8":
-    print("8")
-elif taskArr[0] == "task_9":
-    print("9")
-elif taskArr[0] == "task_10":
-    print("10")
+input("hello")
 
